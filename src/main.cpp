99a98f3ee980fc6193de3e97f4fb7d07161ae486
Robin Voetter <robin@voetter.nl>
2018-09-11 18:25:56 +0200

    More operator

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@ int main() {
     Matrix<float, 2, 2> a({1, 2, 3, 4});
     Matrix<float, 2, 2> b({5, 6, 7, 8});
 
+    a.negate();
+
     std::cout << a << b << (a * b) << std::endl;
 
     return 0;
--- src/math/Matrix.h
+++ src/math/Matrix.h
@@ -4,6 +4,7 @@
 #include <cstddef>
 #include <array>
 #include <ostream>
+#include <type_traits>
 
 template <typename T, size_t R, size_t C>
 class Matrix;
@@ -68,85 +69,83 @@ public:
         return this->derived();
     }
 
-    template <typename U>
-    auto operator+(const Matrix<U, Rows, Cols>& other) {
-        using Result = decltype(std::declval<T>() + std::declval<U>());
-        Matrix<Result, Rows, Cols> result;
-
+    template <typename U, typename V>
+    static void add(Derived& dst, const Matrix<U, Rows, Cols>& l, const Matrix<V, Rows, Cols>& r) {
         for (size_t i = 0; i < Size; ++i)
-            result[i] = (*this)[i] + other[i];
-
-        return result;
+            dst[i] = l[i] + r[i];
     }
 
-    template <typename U>
-    auto operator+=(const Matrix<U, Rows, Cols>& other) {
+    template <typename U, typename V>
+    static void sub(Derived& dst, const Matrix<U, Rows, Cols>& l, const Matrix<V, Rows, Cols>& r) {
         for (size_t i = 0; i < Size; ++i)
-            (*this)[i] += other[i];
-
-        return this->derived();
+            dst[i] = l[i] - r[i];
     }
 
-    template <typename U>
-    auto operator-(const Matrix<U, Rows, Cols>& other) {
-        using Result = decltype(std::declval<T>() + std::declval<U>());
-        Matrix<Result, Rows, Cols> result;
-
-        for (size_t i = 0; i < Size; ++i)
-            result[i] = (*this)[i] - other[i];
-
-        return result;
-    }
-
-    template <typename U>
-    auto operator-() {
-        using Result = decltype(std::declval<T>() + std::declval<U>());
-        Matrix<Result, Rows, Cols> result;
-
-        for (size_t i = 0; i < Size; ++i)
-            result[i] = -(*this)[i];
-        return result;
-    }
-
-    template <typename U>
-    auto operator-=(const Matrix<U, Rows, Cols>& other) {
-        for (size_t i = 0; i < Size; ++i)
-            (*this)[i] -= other[i];
-
-        return this->derived();
-    }
-
-    auto operator*(const T& scalar) {
-        using Result = decltype(std::declval<T>() * scalar);
-        Matrix<Result, Rows, Cols> result;
-
-        for (size_t i = 0; i < Size; ++i)
-            result[i] = (*this)[i] * scalar;
-
-        return result;
-    }
-
-    auto operator*=(const T& scalar) {
-        for (size_t i = 0; i < Size; ++i)
-            (*this)[i] *= scalar;
-
-        return this->derived();
-    }
-
-    template <typename U, size_t OtherCols>
-    auto operator*(const Matrix<U, Cols, OtherCols>& other) {
-        using Result = decltype(std::declval<T>() * std::declval<U>());
-        Matrix<Result, Rows, OtherCols> result({0});
-
-        for (size_t k = 0; k < Cols; ++k) {
+    template <typename U, typename V, size_t N>
+    static void mul(Derived& dst, const Matrix<U, Rows, N>& l, const Matrix<V, N, Cols>& r) {
+        Derived result({0});
+        for (size_t k = 0; k < N; ++k) {
             for (size_t i = 0; i < Rows; ++i) {
-                for (size_t j = 0; j < OtherCols; ++j) {
-                    result(i, j) += (*this)(i, k) * other(k, j);
+                for (size_t j = 0; j < Cols; ++j) {
+                    result(i, j) += l(i, k) * r(k, j);
                 }
             }
         }
 
-        return result;
+        dst = std::move(result);
+    }
+
+    template <typename U>
+    static void neg(Derived& dst, const Matrix<U, Rows, Cols>& src) {
+        for (size_t i = 0; i < Size; ++i)
+            dst[i] = -src[i];
+    }
+
+    template <typename U, typename S>
+    static void scale(Derived& dst, const Matrix<U, Rows, Cols>& src, const S& scalar) {
+        for (size_t i = 0; i < Size; ++i)
+            dst[i] = src[i] * scalar;
+    }
+
+    template <typename U>
+    Derived& operator+=(const Matrix<U, Rows, Cols>& other) {
+        using Result = decltype(std::declval<T>() + std::declval<U>());
+        static_assert(std::is_same<Result, T>::value, "Cannot in-place add these types");
+        add(this->derived(), this->derived(), other);
+        return this->derived();
+    }
+
+    template <typename U>
+    Derived& operator-=(const Matrix<U, Rows, Cols>& other) {
+        using Result = decltype(std::declval<T>() + std::declval<U>());
+        static_assert(std::is_same<Result, T>::value, "Cannot in-place subtract these types");
+        sub(this->derived(), this->derived(), other);
+        return this->derived();
+    }
+
+    template <typename U>
+    Derived& operator*=(const Matrix<U, Rows, Cols>& other) {
+        static_assert(Rows == Cols, "Cannot in-place multiply these matrices");
+        using Result = decltype(std::declval<T>() + std::declval<U>());
+        static_assert(std::is_same<Result, T>::value, "Cannot in-place multiply these types");
+        mul(this->derived(), this->derived(), other);
+        return this->derived();
+    }
+
+    template <typename U>
+    Derived& operator*=(const U& scalar) {
+        static_assert(Rows == Cols, "Cannot in-place multiply these matrices");
+        using Result = decltype(std::declval<T>() * std::declval<U>());
+        static_assert(std::is_same<Result, T>::value, "Cannot in-place scale these types");
+        scale(this->derived(), this->derived(), scalar);
+        return this->derived();
+    }
+
+    Derived& negate() {
+        using Result = decltype(-std::declval<T>());
+        static_assert(std::is_same<T, Result>::value, "Cannot in-place negate this type");
+        neg(this->derived(), this->derived());
+        return this->derived();
     }
 
 private:
@@ -158,6 +157,46 @@ private:
         return *static_cast<Derived*>(this);
     }
 };
+
+template <typename T, typename U, size_t M, size_t N>
+auto operator+(const Matrix<T, M, N>& l, const Matrix<U, M, N>& r) {
+    using Result = decltype(std::declval<T>() + std::declval<U>());
+    Matrix<Result, M, N> result;
+    Matrix<Result, M, N>::add(result, l, r);
+    return result;
+}
+
+template <typename T, typename U, size_t M, size_t N>
+auto operator-(const Matrix<T, M, N>& l, const Matrix<U, M, N>& r) {
+    using Result = decltype(std::declval<T>() + std::declval<U>());
+    Matrix<Result, M, N> result;
+    Matrix<Result, M, N>::sub(result, l, r);
+    return result;
+}
+
+template <typename T, typename U, size_t M, size_t N, size_t O>
+auto operator*(const Matrix<T, M, N>& l, const Matrix<U, N, O>& r) {
+    using Result = decltype(std::declval<T>() * std::declval<U>());
+    Matrix<Result, M, O> result;
+    Matrix<Result, M, O>::mul(result, l, r);
+    return result;
+}
+
+template <typename T, typename U, size_t M, size_t N>
+auto operator*(const Matrix<T, M, N>& src, const U& scalar) {
+    using Result = decltype(std::declval<T>() * std::declval<U>());
+    Matrix<Result, M, N> result;
+    Matrix<Result, M, N>::scale(result, src, scalar);
+    return result;
+}
+
+template <typename T, typename U, size_t M, size_t N>
+auto operator-(const Matrix<T, M, N>& src) {
+    using Result = decltype(-std::declval<T>());
+    Matrix<Result, M, N> result;
+    Matrix<Result, M, N>::neg(result, src);
+    return result;
+}
 
 template <typename Os, typename T, size_t R, size_t C>
 Os& operator<<(Os& os, const Matrix<T, R, C>& mat) {
