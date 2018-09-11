e7df3cedaf102ccc3e038047f5154f5811cecf02
Robin Voetter <robin@voetter.nl>
2018-09-11 15:56:23 +0200

    more matrix ops

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,10 @@
 #include "math/Vector.h"
 
 int main() {
+    Matrix<float, 2, 2> a({1, 2, 3, 4});
+    Matrix<float, 2, 2> b({5, 6, 7, 8});
+
+    std::cout << a << b << (a * b) << std::endl;
+
     return 0;
 }
--- src/math/Matrix.h
+++ src/math/Matrix.h
@@ -3,6 +3,7 @@
 
 #include <cstddef>
 #include <array>
+#include <ostream>
 
 template <typename T, size_t R, size_t C>
 class Matrix;
@@ -17,9 +18,10 @@ public:
     constexpr const static size_t Rows = R;
     constexpr const static size_t Cols = C;
 
-protected:
+private:
     std::array<T, Size> elements;
 
+public:
     MatrixBase() = default;
 
     MatrixBase(const Derived& other):
@@ -38,7 +40,6 @@ protected:
         elements(std::move(elements)) {
     }
 
-private:
     T& operator()(size_t row, size_t col) {
         return this->elements[col * Rows + row];
     }
@@ -54,7 +55,120 @@ private:
     const T& operator[](size_t i) const {
         return this->elements[i];
     }
+
+    template <typename U>
+    Derived& operator=(const Matrix<U, Rows, Cols>& other) {
+        this->elements = other.elements;
+        return this->derived();
+    }
+
+    template <typename U>
+    Derived& operator=(Matrix<U, Rows, Cols>&& other) {
+        this->elements = std::move(other.elements);
+        return this->derived();
+    }
+
+    template <typename U>
+    auto operator+(const Matrix<U, Rows, Cols>& other) {
+        using Result = decltype(std::declval<T>() + std::declval<U>());
+        Matrix<Result, Rows, Cols> result;
+
+        for (size_t i = 0; i < Size; ++i)
+            result[i] = (*this)[i] + other[i];
+
+        return result;
+    }
+
+    template <typename U>
+    auto operator+=(const Matrix<U, Rows, Cols>& other) {
+        for (size_t i = 0; i < Size; ++i)
+            (*this)[i] += other[i];
+
+        return this->derived();
+    }
+
+    template <typename U>
+    auto operator-(const Matrix<U, Rows, Cols>& other) {
+        using Result = decltype(std::declval<T>() + std::declval<U>());
+        Matrix<Result, Rows, Cols> result;
+
+        for (size_t i = 0; i < Size; ++i)
+            result[i] = (*this)[i] - other[i];
+
+        return result;
+    }
+
+    template <typename U>
+    auto operator-() {
+        using Result = decltype(std::declval<T>() + std::declval<U>());
+        Matrix<Result, Rows, Cols> result;
+
+        for (size_t i = 0; i < Size; ++i)
+            result[i] = -(*this)[i];
+        return result;
+    }
+
+    template <typename U>
+    auto operator-=(const Matrix<U, Rows, Cols>& other) {
+        for (size_t i = 0; i < Size; ++i)
+            (*this)[i] -= other[i];
+
+        return this->derived();
+    }
+
+    auto operator*(const T& scalar) {
+        using Result = decltype(std::declval<T>() * scalar);
+        Matrix<Result, Rows, Cols> result;
+
+        for (size_t i = 0; i < Size; ++i)
+            result[i] = (*this)[i] * scalar;
+
+        return result;
+    }
+
+    auto operator*=(const T& scalar) {
+        for (size_t i = 0; i < Size; ++i)
+            (*this)[i] *= scalar;
+
+        return this->derived();
+    }
+
+    template <typename U, size_t OtherCols>
+    auto operator*(const Matrix<U, Cols, OtherCols>& other) {
+        using Result = decltype(std::declval<T>() * std::declval<U>());
+        Matrix<Result, Rows, OtherCols> result({0});
+
+        for (size_t k = 0; k < Cols; ++k) {
+            for (size_t i = 0; i < Rows; ++i) {
+                for (size_t j = 0; j < OtherCols; ++j) {
+                    result(i, j) += (*this)(i, k) * other(k, j);
+                }
+            }
+        }
+
+        return result;
+    }
+
+private:
+    Derived& derived() {
+        return *static_cast<Derived*>(this);
+    }
+
+    const Derived& derived() const {
+        return *static_cast<Derived*>(this);
+    }
 };
+
+template <typename Os, typename T, size_t R, size_t C>
+Os& operator<<(Os& os, const Matrix<T, R, C>& mat) {
+    for (size_t i = 0; i < C; ++i) {
+        for (size_t j = 0; j < R; ++j)
+            os << mat(i, j) << ' ';
+        os << std::endl;
+    }
+
+    return os;
+}
 
 template <typename T, size_t R, size_t C>
 class Matrix: public MatrixBase<T, R, C> {
